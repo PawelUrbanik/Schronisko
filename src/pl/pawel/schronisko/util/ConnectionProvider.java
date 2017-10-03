afc940be7b3ddace3872c97a4bebece7445fb24c
Pawel Urbanik <pawelurbanik94@gmail.com>
2017-10-03 09:44:12 +0200

    add Connection to database

---
+++ src/pl/pawel/schronisko/util/ConnectionProvider.java
@@ -0,0 +1,34 @@
+package pl.pawel.schronisko.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    private static DataSource dataSource;
+
+    public static Connection getConnection() throws SQLException {
+       return getDataSource().getConnection();
+    }
+
+    public static DataSource getDataSource()
+    {
+        if(dataSource==null)
+        {
+            try
+            {
+                Context initialContext = new InitialContext();
+                Context envContext = (Context) initialContext.lookup("java:comp/env");
+                DataSource ds =(DataSource) envContext.lookup("jdbc/schronisko");
+                dataSource= ds;
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+}
